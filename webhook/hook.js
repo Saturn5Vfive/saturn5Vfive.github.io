ef0af61f1fa602bdf797bac61eea98d900fba83b
Saturn5Vfive <58058856+Saturn5Vfive@users.noreply.github.com>
2021-12-12 03:07:56 -0600

    Update hook.js

--- webhook/hook.js
+++ webhook/hook.js
@@ -1,8 +1,8 @@
 function fireserver(){
-    var link = document.getElementById("whurl").value();
-    var username = document.getElementById("username").value();
-    var content = document.getElementById("content").value();
-    var avatar = document.getElementById("avurl").value();
+    var link = document.getElementById("whurl").value;
+    var username = document.getElementById("username").value;
+    var content = document.getElementById("content").value;
+    var avatar = document.getElementById("avurl").value;
     postData(link, {"content": content, "username": username, "avatar_url": avatar});
 }
 
